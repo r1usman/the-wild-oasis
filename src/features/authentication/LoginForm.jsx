714e45a4c417af2dedcb9ab2f159fdfcf8b1b6bf
r1usman <ru0300usman@gmail.com>
2025-06-14 15:33:55 +0500

    Update LoginForm.jsx

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("usman@example.com");
-  const [password, setPassword] = useState("fakefake");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
