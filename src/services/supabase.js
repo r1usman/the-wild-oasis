6301b74519fa7970a24e84dac43f7536aa4ab1e6
usman <ru0300usman@gmail.com>
2025-12-09 21:14:41 +0500

    supabase key added

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://edhasunsosidmqxhyubr.supabase.co";
-const supabaseKey = "sb_publishable_Jcj7E0R6yf6jdNl-RSrk_Q_z0_hQ19Y";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkaGFzdW5zb3NpZG1xeGh5dWJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQzNjU4NTMsImV4cCI6MjAwOTk0MTg1M30.xW3MPoJzyZKVJ9zo1PviWyjVMJUuSS51D_0g4D36how";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
